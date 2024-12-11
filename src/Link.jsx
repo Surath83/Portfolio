414dec137cf30cc62298dc67ff716f31609bfd21
SURATH CHOWDHURY <133858119+Surath83@users.noreply.github.com>
2024-12-12 01:06:45 +0530

    Update Link.jsx

--- src/Link.jsx
+++ src/Link.jsx
@@ -6,7 +6,7 @@ function Link({ title, symbol, link }) {
         if (link) {
             window.open(link, '_blank');
         } else if (title.toLowerCase() === 'whatsapp') {
-            const phoneNumber = "9007418947";
+            const phoneNumber = "+919007418947";
             const message = "nice to meet you";
             const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
             window.open(whatsappUrl, '_blank');
